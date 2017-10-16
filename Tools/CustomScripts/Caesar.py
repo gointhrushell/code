ae0ce12ca3eb66110582e7a05669c0e2f108818f
Austin <austinmcwhirter@gmail.com>
2017-10-16 13:05:35 -0500

    Adding new file structure for custom scripts

---
+++ Tools/CustomScripts/Caesar.py
@@ -0,0 +1,13 @@
+string = input('Input your string: ')
+
+for i in range(1,26):
+    output = ''
+    for j in string:
+        if j != ' ' and j.islower():
+            output += chr((ord(j)-ord('a')+i)%26+ord('a'))
+        elif j != ' ' and j.isupper():
+            output += chr((ord(j)-ord('A')+i)%26+ord('A'))
+        else:
+            output += ' '
+    print(str(i)+". " + output)
+    
---
+++ Tools/CustomScripts/ListScripts.py
@@ -0,0 +1,18 @@
+import glob
+import os 
+dir_path = os.path.dirname(os.path.realpath(__file__))
+
+fileList = glob.glob(dir_path+'\*.py')
+for i in range(0,len(fileList)):
+    fileList[i] = fileList[i].replace(dir_path+'\\','')
+myDescripts={}
+
+with open('C:\\Users\\Austin\\Desktop\\Tools\\CustomScripts\\descriptions.txt','r') as f:
+    descriptions = f.readlines()
+    for line in descriptions:
+        myDescripts[line.split(':')[0]] = line.split(':')[1].strip()
+
+for name in fileList:
+    if name[:-3] != 'ListScripts':
+        print(name[:-3] + ' - ' + myDescripts[name[:-3]])
+        
