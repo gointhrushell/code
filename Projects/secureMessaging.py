15c7e8f423a295053ccaf6ef8fc5f6a3f83c2973
Austin <austin@localhost.localdomain>
2017-12-27 20:33:07 -0600

    Created a library for sending messages with time-based verification

---
+++ Projects/secureMessaging.py
@@ -0,0 +1,76 @@
+#!/usr/bin/python3
+import datetime
+import hashlib
+import socket
+
+class Hasher:
+    
+    def __init__(self,secret):
+        self.secret = secret;
+        self.hasher = hashlib.sha256()
+        
+    def renew(self):
+        del self.hasher
+        self.hasher=hashlib.sha256()        
+        
+    def genSeed(self,delta):
+        seed = self.roundTime(datetime.datetime.now())+delta
+        seed = str(seed) + str(self.secret)
+        return seed
+
+    def roundTime(self,time):
+        return time+(datetime.datetime.min-time)%datetime.timedelta(seconds=30)
+    
+    def genHash(self,delta=datetime.timedelta(seconds=0)):
+        seed = self.genSeed(delta)
+        self.hasher.update(bytes(seed,'utf8'))
+        myhash = self.hasher.hexdigest()
+        self.renew()
+        return myhash
+        
+    def verify(self,message):
+        if self.genHash() == message:
+            return True
+        if self.genHash(-datetime.timedelta(seconds=30))==message:
+            return True
+        if self.genHash(datetime.timedelta(seconds=30))==message:
+            return True
+        return False
+
+class Messenger:
+    def __init__(self,conn,secret):
+        self.conn = conn
+        self.hasher = SecureMessage(secret)
+        
+    def close(self):
+        self.conn.close()
+        
+    def setTimeout(self,time):
+        self.conn.settimeout(time)
+    
+    def send(self,message):
+        self.setTimeout(3)
+        try:
+            self.conn.sendall(bytes(message+self.hasher.genHash(),'utf-8'))
+        except:
+            print("Message failed to send")
+            raise
+    
+    def verify(self,message_hash):
+        return self.hasher.verify(message_hash)
+    
+    
+class Client(Messenger):
+    def __init__(self,server_IP,server_PORT,secret):
+        
+        self.conn = socket.socket()
+        try:
+            self.conn.connect((server_IP,server_PORT))
+        except:
+            print("Unable to make a connection to the server")
+            raise   
+        Messenger.__init__(self,self.conn,secret)
+
+if __name__=="__main__":
+    print("This is only a library file")
+    
