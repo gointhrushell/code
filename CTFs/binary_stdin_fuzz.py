20bc3fd2bbce38c761e6cc719f5b0281476e7e9f
wolfpack270 <austinmcwhirter@gmail.com>
2018-04-11 12:07:27 -0500

    better help info on the fuzzer

--- CTFs/binary_stdin_fuzz.py
+++ CTFs/binary_stdin_fuzz.py
@@ -8,7 +8,14 @@ LOGFILE = '/tmp/fuzz.log'
 SUCCESSFILE = '/tmp/exploit.log'
 VERBOSE = 0
 
-ruby_pattern = 'Aa0Aa1Aa2Aa3Aa4Aa5Aa6Aa7Aa8Aa9Ab0Ab1Ab2Ab3Ab4Ab5Ab6Ab7Ab8Ab9Ac0Ac1Ac2Ac3Ac4Ac5Ac6Ac7Ac8Ac9Ad0Ad1Ad2Ad3Ad4Ad5Ad6Ad7Ad8Ad9Ae0Ae1Ae2Ae3Ae4Ae5Ae6Ae7Ae8Ae9Af0Af1Af2Af3Af4Af5Af6Af7Af8Af9Ag0Ag1Ag2Ag3Ag4Ag5Ag6Ag7Ag8Ag9Ah0Ah1Ah2Ah3Ah4Ah5Ah6Ah7Ah8Ah9Ai0Ai1Ai2Ai3Ai4Ai5Ai6Ai7Ai8Ai9Aj0Aj1Aj2Aj3Aj4Aj5Aj6Aj7Aj8Aj9Ak0Ak1Ak2Ak3Ak4Ak5Ak6Ak7Ak8Ak9Al0Al1Al2Al3Al4Al5Al6Al7Al8Al9Am0Am1Am2Am3Am4Am5Am6Am7Am8Am9An0An1An2An3An4An5An6An7An8An9Ao0Ao1Ao2Ao3Ao4Ao5Ao6Ao7Ao8Ao9Ap0Ap1Ap2Ap3Ap4Ap5Ap6Ap7Ap8Ap9Aq0Aq1Aq2Aq3Aq4Aq5Aq6Aq7Aq8Aq9Ar0Ar1Ar2Ar3Ar4Ar5Ar6Ar7Ar8Ar9As0As1As2As3As4As5As6As7As8As9At0At1At2At3At4At5At6At7At8At9Au0Au1Au2Au3Au4Au5Au6Au7Au8Au9Av0Av1Av2Av3Av4Av5Av6Av7Av8Av9Aw0Aw1Aw2Aw3Aw4Aw5Aw6Aw7Aw8Aw9Ax0Ax1Ax2Ax3Ax4Ax5Ax6Ax7Ax8Ax9Ay0Ay1Ay2Ay3Ay4Ay5Ay6Ay7Ay8Ay9Az0Az1Az2Az3Az4Az5Az6Az7Az8Az9Ba0Ba1Ba2Ba3Ba4Ba5Ba'
+ruby_pattern = 'Aa0Aa1Aa2Aa3Aa4Aa5Aa6Aa7Aa8Aa9Ab0Ab1Ab2Ab3Ab4Ab5Ab6Ab7Ab8Ab9Ac0Ac1Ac2Ac3Ac4Ac5Ac6Ac7Ac8A' \
+    'c9Ad0Ad1Ad2Ad3Ad4Ad5Ad6Ad7Ad8Ad9Ae0Ae1Ae2Ae3Ae4Ae5Ae6Ae7Ae8Ae9Af0Af1Af2Af3Af4Af5Af6Af7Af8Af9Ag0Ag1Ag' \
+    '2Ag3Ag4Ag5Ag6Ag7Ag8Ag9Ah0Ah1Ah2Ah3Ah4Ah5Ah6Ah7Ah8Ah9Ai0Ai1Ai2Ai3Ai4Ai5Ai6Ai7Ai8Ai9Aj0Aj1Aj2Aj3Aj4Aj5A' \ 
+    'j6Aj7Aj8Aj9Ak0Ak1Ak2Ak3Ak4Ak5Ak6Ak7Ak8Ak9Al0Al1Al2Al3Al4Al5Al6Al7Al8Al9Am0Am1Am2Am3Am4Am5Am6Am7Am8Am9A' \
+    'n0An1An2An3An4An5An6An7An8An9Ao0Ao1Ao2Ao3Ao4Ao5Ao6Ao7Ao8Ao9Ap0Ap1Ap2Ap3Ap4Ap5Ap6Ap7Ap8Ap9Aq0Aq1Aq2Aq3' \
+    'Aq4Aq5Aq6Aq7Aq8Aq9Ar0Ar1Ar2Ar3Ar4Ar5Ar6Ar7Ar8Ar9As0As1As2As3As4As5As6As7As8As9At0At1At2At3At4At5At6At7' \
+    'At8At9Au0Au1Au2Au3Au4Au5Au6Au7Au8Au9Av0Av1Av2Av3Av4Av5Av6Av7Av8Av9Aw0Aw1Aw2Aw3Aw4Aw5Aw6Aw7Aw8Aw9Ax0Ax1' \
+    'Ax2Ax3Ax4Ax5Ax6Ax7Ax8Ax9Ay0Ay1Ay2Ay3Ay4Ay5Ay6Ay7Ay8Ay9Az0Az1Az2Az3Az4Az5Az6Az7Az8Az9Ba0Ba1Ba2Ba3Ba4Ba5Ba'
 
 
 organized_payloads = {}
@@ -24,7 +31,6 @@ organized_payloads['alpha']=['a','A','z','Z']
 organized_payloads['digits']=['-1','0','1','9']
 
 pattern = re.compile(r'(?:Segmentation fault)|(?:sigsev)|(?:stack smashing)|(?:\[.*?\] (.*?)\[.*?\].*?segfault at [0-9a-fA-F]+ ip ([0-9a-fA-F]+))',flags=re.I)
-#shell_pattern=re.compile(r'shell|bash|execute',flags=re.I)
 format_pattern=re.compile(r'[0-9a-fA-F]{18}',flags=re.I)
 hexin = re.compile(r'(\\x)([0-9a-fA-F]{2})')
 
@@ -157,8 +163,8 @@ def stdinInput(path,pay_type,payload,new_baseline):
                     print(text)
                     
                     if pay_type != 'bof':
-                        logger(text+"\n"+f'Payload: {binascii.unhexlify(binascii.hexlify(bytes(payload,"utf-8")))}\n',1)
-                    else: # if its from a bof, print the minified payload
+                        logger(text+"\n"+f'Payload: {}\n',1)
+                    else:
                         logger(text+"\n"+f'Offset: {pattern_offset}\n',1)
                         bof_exception = BOF()
                         raise bof_exception
@@ -232,7 +238,7 @@ def main():
         
 if __name__ == '__main__':
     a = argparse.ArgumentParser()
-    a.add_argument('-p','--path',type=str,help='Directory or file to fuzz',action='store',dest='path',required=True)
+    a.add_argument('-p','--path',type=str,help='Directory or file to fuzz [NOTE: Do not use /* just give a directory]',action='store',dest='path',required=True)
     a.add_argument('-e','--ext',type=str,help='File extension to use (.txt,.exe,.elf)',action='store',dest='ext')
     a.add_argument('--pre',help='String input before payload. Useful if you have to nagivate a menu (interprets pythonic \\n, \\t and \\xFF)',type=str,action='store',dest='pre')
     a.add_argument('--post',help='String input after payload. Useful if you have to trigger the exploit (interprets pythonic \\n, \\t and \\xFF)',type=str,action='store',dest='post')
