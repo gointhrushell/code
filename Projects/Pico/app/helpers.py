56457e3064db3cc313ebf31c3191459f1faf691e
unknown <austinmcwhirter@gmail.com>
2018-06-19 18:33:52 -0500

    Adding User and Card object support

--- Projects/Pico/app/helpers.py
+++ Projects/Pico/app/helpers.py
@@ -1,7 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import DataRequired,length
+from wtforms.validators import DataRequired,length,EqualTo,ValidationError
 from werkzeug.security import generate_password_hash,check_password_hash
+from app.models import User
 
 
 
@@ -11,11 +12,24 @@ class LoginForm(FlaskForm):
     remember_me = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
     
+class RegisterForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired(),length(min=1,max=32)])
+    password = PasswordField('Password', validators=[DataRequired(),length(min=1, max=32)])
+    password2 = PasswordField('Confirm Password', validators=[DataRequired(),EqualTo('password'),length(max=32)])
+    submit= SubmitField('Register')
+    
+    def validate_username(self,username):
+        user = User.query.filter_by(username=username.data).first()
+        if user is not None:
+            raise ValidationError("Please pick a different username")
+        
+    
+    
 class CardForm(FlaskForm):
-    title = StringField("Card Title",validators=[DataRequired(),length(max=32)])
+    #title = StringField("Card Title",validators=[DataRequired(),length(max=32)])
     question = StringField("Question",validators=[DataRequired(),length(max=256)])
     answer = StringField("Answer",validators=[DataRequired(),length(max=256)])
-    submit = SubmitField('Sign In')
+    submit = SubmitField('Create')
     
 
 
--- Projects/Pico/app/models.py
+++ Projects/Pico/app/models.py
@@ -1,26 +1,32 @@
-from app import db
+from app import db,login
+from werkzeug.security import generate_password_hash,check_password_hash
+from flask_login import UserMixin
 
-class User(db.Model):
+@login.user_loader
+def load_user(id):
+    return User.query.get(int(id))
+
+
+class User(UserMixin, db.Model):
     
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(64),index=True,unique=True)
     password_hash = db.Column(db.String(128))
     comments = db.Column(db.String(128))
+    admin = db.Column(db.Integer,default=0)
     
-    def __repr__(self):
-        return '<User {}>'.format(self.username)
-    
-    '''def __init__(self, name):
-        self.name = name
-        self.admin = self.is_admin()
-
     def set_password(self,password):
         self.password_hash = generate_password_hash(password)
         
     def check_password(self,password):
-        return check_password_hash(self.password_hash,password)'''
+        return check_password_hash(self.password_hash,password)
+    
+    def __repr__(self):
+        return '<User {}>'.format(self.username)
+    
 
 class Card(db.Model):
+    '''id,question,answer,user_id'''
     id = db.Column(db.Integer,primary_key=True)
     question = db.Column(db.String(256))
     answer = db.Column(db.String(256))
